ef140c7fbbe8be90818bf81dbd941937dd0bda62
mariuble <mariuble@stud.ntnu.no>
2021-08-18 19:50:44 +0200

    add to cart functionality & cart screen

--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -8,7 +8,7 @@ import Loader from '../components/Loader'
 import { listProductDetails } from '../actions/productActions'
 
 const ProductScreen = ({ history, match }) => {
-  const [qty, setQty] = useState(0)
+  const [qty, setQty] = useState(1)
 
   const dispatch = useDispatch()
 
@@ -44,7 +44,7 @@ const ProductScreen = ({ history, match }) => {
               </ListGroup.Item>
               <ListGroup.Item>
                 <Rating
-                  value={product.rating}
+                  value={Number(product.rating)}
                   text={`${product.numReviews} reviews`}
                 />
               </ListGroup.Item>
