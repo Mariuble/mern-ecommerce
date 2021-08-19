c0c1a4ca08757fbbb1ecf12a3dd0d094b84cd118
mariuble <mariuble@stud.ntnu.no>
2021-08-19 20:15:09 +0200

    User Login Screen & Functionality

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -3,9 +3,9 @@ import {
   USER_LOGIN_FAIL,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
-} from '../constants/userConstans'
+} from '../constants/userConstants'
 
-export const login = (email, passwod) => async (dispatch) => {
+export const login = (email, password) => async (dispatch) => {
   try {
     dispatch({
       type: USER_LOGIN_REQUEST,
